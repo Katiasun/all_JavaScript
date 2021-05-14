d6725c6fdd7dc39a7001d63189d458064ef17f81
Catherine <katjatolok3@gmai.com>
2021-05-14 20:45:49 +0200

    done 7 tasks

---
+++ Lesson_2js/js/script.js
@@ -0,0 +1,37 @@
+// 1
+// let yourName = +prompt("Enter you name:");
+// alert(`Привет, ${yourName}`);
+
+// 2
+// let age = +prompt("Enter year of your birth:");
+// const thisYear = 2021;
+// alert(`Your age: ${thisYear - age}`);
+
+// 3
+// let side = +prompt("Enter the side of the square: ");
+// alert(`Perimeter of a square: ${4 * side}`);
+
+// 4
+// let radius = +prompt("Enter radius:");
+// alert(`area of a circle: ${3.14 * radius * radius}`);
+
+// 5
+// let km = +prompt("Enter kilometers:");
+// let time = +prompt("Enter time:");
+// alert(`Your speed: ${km / time}`);
+
+// 6
+// let dollar = +prompt("Enter dollar:");
+// const euro = 0.82;
+// alert(`Convector: ${dollar * euro}`);
+
+// 7
+// let flash = +prompt("Enter the size of the flash drive:");
+// const memory = 820;
+// alert(`Number of files to move: ${(flash * 1000) / memory}`);
+
+// 8
+let money = +prompt("Enter amount of money:");
+let chocolatePrice = +prompt("Enter chocolate price:");
+alert(`Number of chocolates: ${money / chocolatePrice}`);
+alert(`Remainder:`);
