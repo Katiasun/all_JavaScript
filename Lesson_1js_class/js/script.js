079522c8a45372cc6f8b88ee56a8656a46cb766b
Catherine <katjatolok3@gmai.com>
2021-05-20 14:54:45 +0200

    removed // in lesson_1

--- Lesson_1js_class/js/script.js
+++ Lesson_1js_class/js/script.js
@@ -9,65 +9,65 @@ console.log(gretting);
 console.log("Hello you");
 console.log(eyes);
 
-// let name = "Katia";
-// alert(`Hello, ${name}`);
+let name = "Katia";
+alert(`Hello, ${name}`);
 
-// let resultOne = "Frilanser" + " " + "po" + " " + "life";
-// console.log(resultOne);
+let resultOne = "Frilanser" + " " + "po" + " " + "life";
+console.log(resultOne);
 
-// let age = prompt("how are you old?", 100);
-// alert("You ${age} years");
+let age = prompt("how are you old?", 100);
+alert("You ${age} years");
 
-// let myName = prompt("Your name");
-// alert(myName);
+let myName = prompt("Your name");
+alert(myName);
 
-// Classwork
+Classwork;
 // 1
-// let num = +prompt("Enter number:");
-// alert(`${num} squard is ${num ** 2}`);
+let num = +prompt("Enter number:");
+alert(`${num} squard is ${num ** 2}`);
 
 // 2
-// let num1 = +prompt("Enter the first number:");
-// let num2 = +prompt("Enter the second number:");
-// alert(`Average: ${num1 + num2} / 2`);
+let num1 = +prompt("Enter the first number:");
+let num2 = +prompt("Enter the second number:");
+alert(`Average: ${num1 + num2} / 2`);
 
 // 3
-// let side = +prompt("Enter the side of a asquare");
-// alert(` The square's square ${side ** 2}`);
+let side = +prompt("Enter the side of a asquare");
+alert(` The square's square ${side ** 2}`);
 
 // 4
-// let miles_in_kilometer = 0.621371;
-// let distanceInKilometers = +prompt("Enter distance in km:");
-// alert(`The distance in miles is  ${miles_in_kilometer * distanceInKilometers}`);
+let miles_in_kilometer = 0.621371;
+let distanceInKilometers = +prompt("Enter distance in km:");
+alert(`The distance in miles is  ${miles_in_kilometer * distanceInKilometers}`);
 
 // 5
-// let a = +prompt("Enter number first:");
-// let b = +prompt("Enter number second:");
-// alert(`Sum ${a + b}`);
-// alert(`Subtraction ${a - b}`);
-// alert(`Multiplication ${a ** b}`);
-// alert(`Division ${a / b}`);
+let a = +prompt("Enter number first:");
+let b = +prompt("Enter number second:");
+alert(`Sum ${a + b}`);
+alert(`Subtraction ${a - b}`);
+alert(`Multiplication ${a ** b}`);
+alert(`Division ${a / b}`);
 
 // 6
-//  a = +prompt("Enter number a:");
-//  b = +prompt("Enter number b:");
-// alert(`x= ${-b / a}`);
+a = +prompt("Enter number a:");
+b = +prompt("Enter number b:");
+alert(`x= ${-b / a}`);
 
 // 7!
 
 // 8
-// let threeDigitNumber = +prompt("Three digit number:");
-// alert(((threeDigitNumber % 100) - (threeDigitNumber % 10)) / 10);
+let threeDigitNumber = +prompt("Three digit number:");
+alert(((threeDigitNumber % 100) - (threeDigitNumber % 10)) / 10);
 
 // 9
 
-// let fiveDigitNumber = +prompt("Enter five digit number:");
-// let lastNumber = fiveDigitNumber % 10;
-// let withoutLastNumber = (fiveDigitNumber - lastNumber) / 10;
-// alert(` Your number is ${lastNumber * 10000 + withoutLastNumber}`);
+let fiveDigitNumber = +prompt("Enter five digit number:");
+let lastNumber = fiveDigitNumber % 10;
+let withoutLastNumber = (fiveDigitNumber - lastNumber) / 10;
+alert(` Your number is ${lastNumber * 10000 + withoutLastNumber}`);
 
 // 10
-// const SALARY = 250;
-// const salary = +prompt("Enter your month salary:");
-// let newSalary = salary;
-// alert(`Your salary : ${SALARY + salary * 0.1}`);
+const SALARY = 250;
+const salary = +prompt("Enter your month salary:");
+let newSalary = salary;
+alert(`Your salary : ${SALARY + salary * 0.1}`);
