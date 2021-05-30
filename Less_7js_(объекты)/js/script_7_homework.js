f311b35651db1bc3fd61dc43ac4d4abbda3e19b8
Catherine <katjatolok3@gmai.com>
2021-05-30 17:15:36 +0200

    lesson_7_js

--- Less_7js_(объекты)/js/script_7_homework.js
+++ Less_7js_(объекты)/js/script_7_homework.js
@@ -19,13 +19,23 @@
 
 // Функция сокращения объекта-дроби.
 
-let drob1 = { numerator: 4, denominator: 12 };
+let drob1 = { numerator: 5, denominator: 12 };
 let drob2 = { numerator: 2, denominator: 6 };
 
 function findNewDiviser(x, y) {
   let minNumber = Math.min(Math.abs(x), Math.abs(y));
-  for (let i = Math.min; i <= minNumber; i++) {}
+  for (let i = minNumber; i >= 1; i--) {
+    if (x % i === 0 && y % i === 0) {
+      return i;
+    }
+  }
 }
+function cutDrob(result) {
+  let spilnyDilnyk = findNewDiviser(result.numerator, result.denominator);
+  result.numerator /= spilnyDilnyk;
+  result.denominator /= spilnyDilnyk;
+}
+
 function adddmyDrob(x, y) {
   let suma;
   if (x.denominator === y.denominator) {
@@ -36,6 +46,7 @@ function adddmyDrob(x, y) {
       denominator: x.denominator * y.denominator,
     };
   }
+  cutDrob(suma);
   return suma;
 }
 console.log(adddmyDrob(drob1, drob2));
@@ -52,6 +63,7 @@ function multiply(x, y) {
     numerator: x.numerator * y.numerator,
     denominator: x.denominator * y.denominator,
   };
+  cutDrob(product);
   return product;
 }
 console.log(multiply(drob1, drob2));
@@ -61,9 +73,11 @@ function divide(x, y) {
     numerator: x.numerator * y.denominator,
     denominator: x.denominator * y.numerator,
   };
+  cutDrob(quotient);
   return quotient;
 }
 console.log(divide(drob1, drob2));
+
 //3.Создать объект, описывающий время (часы, минуты, секунды), и следующие функции для работы с этим объектом:
 
 // Функция вывода времени на экран;
