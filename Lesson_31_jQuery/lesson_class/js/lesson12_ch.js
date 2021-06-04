183304ead781d5c2af33bf0850de1c3da93c6d70
Anatoly Kalyuzhny <tolibuster21@gmail.com>
2021-06-04 15:18:07 +0300

    Add classwork files

---
+++ Lesson_31_jQuery/lesson_class/js/lesson12_ch.js
@@ -0,0 +1,30 @@
+const $paragraph = $("p");
+$paragraph.addClass("something"); //add class
+paragraph.removeClass("something"); // remove class
+paragraph.hasClass("something"); //sprawdza czy jest taki class
+
+const $button = $("button");
+const $toggleButton = $("button.toggle");
+
+const $ul = $("ul");
+$button.on("click", () => {
+  $paragraph.toggleClass("something");
+  const $li = $("<li>item</li>");
+});
+
+$button.on("click", () => {
+  const $li = $("<li>item</li>");
+  const $deleteButton = $(`<button class=delete>Remove</button>`);
+  $li.append($deleteButton);
+  $ul.append($li);
+});
+
+const $deleteButton = $(`button.delete`);
+
+$(document).on("click", "button.delete", function () {
+  $(this).parents("li").remove();
+});
+
+$toggleButton.on("click", () => {
+  $paragraph.slideToggle(100);
+});
