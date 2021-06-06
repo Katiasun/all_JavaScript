71de592cea46986a22e9908d19b207d0ced0ed44
Catherine <katjatolok3@gmai.com>
2021-06-06 21:25:09 +0200

    added img into html

--- Lesson_31_jQuery/lesson_hw/js/lesson12_ch.js
+++ Lesson_31_jQuery/lesson_hw/js/lesson12_ch.js
@@ -1,30 +1,30 @@
-const $paragraph = $("p");
-$paragraph.addClass("something"); //add class
-paragraph.removeClass("something"); // remove class
-paragraph.hasClass("something"); //sprawdza czy jest taki class
+// const $paragraph = $("p");
+// $paragraph.addClass("something"); //add class
+// paragraph.removeClass("something"); // remove class
+// paragraph.hasClass("something"); //sprawdza czy jest taki class
 
-const $button = $("button");
-const $toggleButton = $("button.toggle");
+// const $button = $("button");
+// const $toggleButton = $("button.toggle");
 
-const $ul = $("ul");
-$button.on("click", () => {
-  $paragraph.toggleClass("something");
-  const $li = $("<li>item</li>");
-});
+// const $ul = $("ul");
+// $button.on("click", () => {
+//   $paragraph.toggleClass("something");
+//   const $li = $("<li>item</li>");
+// });
 
-$button.on("click", () => {
-  const $li = $("<li>item</li>");
-  const $deleteButton = $(`<button class=delete>Remove</button>`);
-  $li.append($deleteButton);
-  $ul.append($li);
-});
+// $button.on("click", () => {
+//   const $li = $("<li>item</li>");
+//   const $deleteButton = $(`<button class=delete>Remove</button>`);
+//   $li.append($deleteButton);
+//   $ul.append($li);
+// });
 
-const $deleteButton = $(`button.delete`);
+// const $deleteButton = $(`button.delete`);
 
-$(document).on("click", "button.delete", function () {
-  $(this).parents("li").remove();
-});
+// $(document).on("click", "button.delete", function () {
+//   $(this).parents("li").remove();
+// });
 
-$toggleButton.on("click", () => {
-  $paragraph.slideToggle(100);
-});
+// $toggleButton.on("click", () => {
+//   $paragraph.slideToggle(100);
+// });
